b2f022a0485361cf1d18d1076d08d036fbac5ff1
Mohamed Magdy <moh.magdy40@gmail.com>
2018-10-31 20:20:08 +0200

    [ADD] Add the state for the workflow and the methods to be triggered in the models and the buttons in the XML views.

--- models/models.py
+++ models/models.py
@@ -13,3 +13,22 @@ class CarRequest(models.Model):
     date_to = fields.Datetime(string="End Date", required=False, )
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
     car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
+    state = fields.Selection(string="Status", selection=[('draft', 'Draft'), ('confirm', 'Confirm'),
+                                                         ('validate', 'Validated'), ('refuse', 'Refuse'),
+                                                         ('approved', 'Approved'), ], default="draft", track_visibility='onchange', )
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+
+    @api.multi
+    def approve_request(self):
+        self.state = 'approved'
