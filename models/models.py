792f945020e4492ca93e632eb2ac1ee0896c9751
Mohamed Magdy <moh.magdy40@gmail.com>
2018-10-31 19:39:31 +0200

    [ADD] Add the model, view, action and menus for the car request.

--- models/models.py
+++ models/models.py
@@ -2,14 +2,13 @@
 
 from odoo import models, fields, api
 
-# class employee_car_request(models.Model):
-#     _name = 'employee_car_request.employee_car_request'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class CarRequest(models.Model):
+    _name = "car.request" # Table in DB => car_request
+    _description = "Car Request"
+
+    name = fields.Char(string="Request", required=True, )
+    date_from = fields.Datetime(string="Starting Date", default=fields.Datetime.now(), )
+    date_to = fields.Datetime(string="End Date", required=False, )
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
+    car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
