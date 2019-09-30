e76c3897428f6eabab047961bcf976f1db02afc2
Mohamed Magdy <moh.magdy40@gmail.com>
2019-09-30 08:10:36 +0200

    [ADD] Add website as a dependency

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr', 'fleet', ],
+    'depends': ['base', 'hr', 'fleet', 'website', ],
 
     # always loaded
     'data': [
